0c09d383aa1deca250b6e1341a0b71e36e323258
chaitanya-online <chaithu.nampalli@gmail.com>
2022-07-15 18:16:38 +0530

    Added few changes

--- src/main/java/com/example/jsonpath/JsonPathApplication.java
+++ src/main/java/com/example/jsonpath/JsonPathApplication.java
@@ -1,23 +1,13 @@
 package com.example.jsonpath;
 
 
-
-import com.example.jsonpath.service.ReadDataFromJsonFile;
-import net.minidev.json.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-
-import java.io.FileReader;
-
 
 @SpringBootApplication
 public class JsonPathApplication {
 
-    public static void main(String[] args) throws  Exception{
+    public static void main(String[] args)  {
 
 
         SpringApplication.run(JsonPathApplication.class, args);
